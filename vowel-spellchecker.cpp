5913f660702f020df82bcce5e304a0a7ca61d9c2
Ensky <enskylin@gmail.com>
2020-09-20 11:45:16 +0800

    Update vowel-spellchecker.cpp
    tolower only once, and used unordered_map

--- vowel-spellchecker.cpp
+++ vowel-spellchecker.cpp
@@ -2,8 +2,8 @@
 /**
  * problem: https://leetcode.com/problems/vowel-spellchecker/
  * result: 
- *   Runtime: 496 ms, faster than 5.04% of C++ online submissions for Vowel Spellchecker.
- *   Memory Usage: 34.5 MB, less than 61.76% of C++ online submissions for Vowel Spellchecker.
+ *   Runtime: 252 ms, faster than 28.99% of C++ online submissions for Vowel Spellchecker.
+ *   Memory Usage: 35.1 MB, less than 34.45% of C++ online submissions for Vowel Spellchecker.
  * used_time: 20min
  */
 
@@ -13,60 +13,63 @@ public:
         this->prepareMatches(wordlist);
         return this->matchQueries(queries);
     }
-    
+
     void prepareMatches(const vector<string>& wordlist) {
         this->wordlist = &wordlist;
         
         int idx = 0;
         for (const auto& word : wordlist) {
             this->updateMatch(this->exactMatch, word, idx);
-            this->updateMatch(this->caseMatch, this->toLower(word), idx);
-            this->updateMatch(this->vowelMatch, this->toVowel(word), idx);
+
+            const string lower = this->toLower(word);
+            this->updateMatch(this->caseMatch, lower, idx);
+            this->updateMatch(this->vowelMatch, this->toVowel(lower), idx);
             idx++;
         }
     }
-    
+
     vector<string> matchQueries(const vector<string>& queries) {
         vector<string> results;
         for (const auto &query : queries) {
             string result = this->matchResult(this->exactMatch, query);
+            string lower;
             if (result.empty()) {
-                result = this->matchResult(this->caseMatch, this->toLower(query));
+                lower = this->toLower(query);
+                result = this->matchResult(this->caseMatch, lower);
             }
             if (result.empty()) {
-                result = this->matchResult(this->vowelMatch, this->toVowel(query));
+                result = this->matchResult(this->vowelMatch, this->toVowel(lower));
             }
             results.emplace_back(std::move(result));
         }
         return results;
     }
-    
-    void updateMatch(map<string, int>& matchMap, const string& word, int idx) {
+
+    void updateMatch(unordered_map<string, int>& matchMap, const string& word, int idx) {
         if (matchMap.count(word) > 0) {
             return;
         }
         matchMap[word] = idx;        
     }
-    
-    string matchResult(const map<string, int>& matchMap, const string& query) {
+
+    string matchResult(const unordered_map<string, int>& matchMap, const string& query) {
         auto it = matchMap.find(query);
         if (it == matchMap.end()) {
             return "";
         }
         return this->wordlist->at(it->second);
     }
-    
+
     string toLower(const std::string& str) {
         string result;
         std::transform(str.begin(), str.end(), std::back_inserter(result), 
                        [](char c ){ return std::tolower(c); });
         return result;
     }
-    
+
     string toVowel(const std::string& str) {
-        string newstr = toLower(str);
         string result;
-        for (auto c : newstr) {
+        for (auto c : str) {
             if (this->isVowel(c)) {
                 result.push_back('|');
             } else {
@@ -82,7 +85,7 @@ public:
     
 private:
     const vector<string>* wordlist;
-    map<string, int> exactMatch;
-    map<string, int> caseMatch;
-    map<string, int> vowelMatch;
+    unordered_map<string, int> exactMatch;
+    unordered_map<string, int> caseMatch;
+    unordered_map<string, int> vowelMatch;
 };
